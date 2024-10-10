f53756f21cf073507ae7f9325d71efe060aca4fa
noa <abmagori@gmail.com>
2024-10-10 11:06:58 +0300

    10.10.24 11:05

---
+++ index.js
@@ -0,0 +1,11 @@
+import data from "./data.json" with {type: 'json'}
+
+console.log(data);
+
+// const getProducts = async () => {
+//     const response = await fetch(`https://fakestoreapi.com/products/1`);
+//     const data = await response.json();
+//     console.log(data);
+// }
+// getProducts();
+
