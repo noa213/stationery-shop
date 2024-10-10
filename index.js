f8219a367a7d39237367fb41ebb8bd3048de5cee
noa <abmagori@gmail.com>
2024-10-10 13:24:01 +0300

    10.10.24 13:20

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ import data from "./data.json" with {type: 'json'}
 
 console.log(data);
 
+const notebooks = data.notebooks;
+const crafts = data.crafts;
+const container = document.getElementById('container');
+console.log(notebooks);
+
+notebooks.map(notebook=>{
+    const product = document.createElement('div');
+    product.classList.add('product');
+    product.innerHTML=
+        `<p>${notebook.title}</p>
+        <p>${notebook.price}</p>
+        ${notebook.image}
+        <img src=${notebook.image}>
+
+        `
+        // <img src=${notebook.image}/>
+
+    container.appendChild(product);
+
+})
 // const getProducts = async () => {
 //     const response = await fetch(`https://fakestoreapi.com/products/1`);
 //     const data = await response.json();
