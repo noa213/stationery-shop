435ec5f799544a16090cb9ca80f1de01e07de066
noa <abmagori@gmail.com>
2024-10-13 09:25:53 +0300

    13.10.24 9:25

--- index.js
+++ index.js
@@ -4,21 +4,31 @@ console.log(data);
 
 const notebooks = data.notebooks;
 const crafts = data.crafts;
-const container = document.getElementById('container');
+const container = document.getElementsByClassName('grid')[0];
 console.log(notebooks);
 
-notebooks.map(notebook=>{
-    const product = document.createElement('div');
-    product.classList.add('product');
-    product.innerHTML=
-        `<p>${notebook.title}</p>
-        <p>${notebook.price}</p>
-        <img src=${notebook.image} height='25'>
+notebooks.map(notebook => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML =`
+        <p class='ptitle'>${notebook.title}</p>
+        <img src=${notebook.image} height=100 width=100>
+        <div class='card-footer'>
+            <p class='new-price'>${notebook.price}</p>
+            <p></p>
+            <p></p>
+            <button class='add-btn'>הוספה לסל</button>
+        </div>
         `
-
-    container.appendChild(product);
-
+    container.appendChild(card);
 })
+const num=0;
+document.getElementsByClassName('add-btn').addEventListener('click', function() {
+    console.log('addEventListener');
+    const element = document.getElementById('pay');
+    element.innerText(`(${num++})`);
+});
+
 // const getProducts = async () => {
 //     const response = await fetch(`https://fakestoreapi.com/products/1`);
 //     const data = await response.json();
